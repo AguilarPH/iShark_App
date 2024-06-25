29a39bcb1b3182963099694a26c3288e7e294b03
PabloAguilar <120538250+AguilarPH@users.noreply.github.com>
2024-06-24 23:48:11 -0700

    Added webview locators to NsuEmailText, NsuPassowrdText and NsuNextButton

--- src/main/java/pages/SignInPage.java
+++ src/main/java/pages/SignInPage.java
@@ -23,15 +23,18 @@ public class SignInPage extends BasePage{
     public WebElement guestIngress() {return setGuestIngress;}
 
     @AndroidFindBy(className = "android.widget.EditText")
+    @AndroidFindBy(xpath = "//input[@id='i0116']")
     private WebElement setNsuEmailText;
     public WebElement nsuEmailText() {return setNsuEmailText;}
 
     @AndroidFindBy(xpath = "//android.widget.Button[@text='Next']")
+    @AndroidFindBy(xpath = "//input[@id='idSIButton9']")
     private WebElement setNsuNextButton;
     public WebElement nsuNextButton() {return setNsuNextButton;}
 
     @AndroidFindBy(xpath = "//android.widget.TextView[@text='Enter password']/parent::android.view.View" +
             "/following-sibling::*[1]/descendant::android.widget.EditText")
+    @AndroidFindBy(xpath = "//input[@id='i0118']")
     private WebElement setNsuPasswordText;
     public WebElement nsuPasswordText() {return setNsuPasswordText;}
 
