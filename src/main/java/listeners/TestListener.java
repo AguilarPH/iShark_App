a63acce15cbbc5833445c0bea477a2f34051bc85
PabloAguilar <120538250+AguilarPH@users.noreply.github.com>
2024-08-08 12:40:34 -0700

    in onTestFailure listener method, added a screenshot take to keep as evidence

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -1,18 +1,32 @@
 package listeners;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import steps.BaseSteps;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class TestListener implements  ITestListener {
+    BaseSteps baseSteps = new BaseSteps();
+
     public void onTestFailure(ITestResult result) {
         if (result.getThrowable() != null) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             result.getThrowable().printStackTrace(pw);
             System.out.println(sw.toString());
+        }
+
+        File file = baseSteps.getDriver().getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("FailScr.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
