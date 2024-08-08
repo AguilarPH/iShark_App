bd61fec20ddcb8f5df165c8affc24a7fae724d95
PabloAguilar <120538250+AguilarPH@users.noreply.github.com>
2024-08-08 13:30:00 -0700

    Added a screenshot take to the onTestFailure listener saving it in custom folder according to the device used, the failed test and the date and time

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -10,6 +10,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestListener implements  ITestListener {
     BaseSteps baseSteps = new BaseSteps();
@@ -23,6 +25,13 @@ public class TestListener implements  ITestListener {
         }
 
         File file = baseSteps.getDriver().getScreenshotAs(OutputType.FILE);
+
+        Map<String, String> params = new HashMap<String, String>();
+        params = result.getTestContext().getCurrentXmlTest().getAllParameters();
+
+        String imgPath = "Screenshots" + File.separator + params.get("platformName") + "_" +params.get("platformVersion")
+                + "_" + params.get("deviceName") + File.separator + baseSteps.getDateTime() + File.separator +
+                result.getTestClass().getRealClass().getSimpleName() + File.separator + result.getName() + ".png";
         try {
             FileUtils.copyFile(file, new File("FailScr.png"));
         } catch (IOException e) {
